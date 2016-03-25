db75c69cd23126b232c932f0719c2f7a71eb29e5
Nick Monk <nick@monk.software>
2016-03-25 22:53:43 +0000

    Inital project setup

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject teamlinkup "0.0.1-SNAPSHOT"
+  :description "TeamLinkUp"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.omcljs/om "1.0.0-alpha30"]
+                 [datascript "0.13.1"]
+                 [figwheel-sidecar "0.5.0-6" :scope "test"]])
